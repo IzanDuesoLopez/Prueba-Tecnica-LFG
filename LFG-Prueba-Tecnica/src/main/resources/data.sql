ed0f944f9970b24fcebd6fafdf480c381d3fc935
IzanDuesoLopez <izandueso@gmail.com>
2022-08-23 10:04:52 +0200

    DML & DDL

---
+++ LFG-Prueba-Tecnica/src/main/resources/data.sql
@@ -0,0 +1,52 @@
+drop table if exists users_in_parties, parties, groups, users;
+
+create table users (
+	id int auto_increment primary key,
+    username varchar(20),
+    password varchar(16),
+    steam_username varchar(20),
+    user_rol varchar(20)
+);
+
+create table groups (
+	id int auto_increment primary key,
+    videogame varchar(50),
+	members int,
+    user_id int,
+    constraint group_fk foreign key (user_id) references users (id)
+    on delete cascade on update cascade
+);
+
+create table parties (
+	id int auto_increment primary key,
+    members int,
+    messages int
+);
+
+create table users_in_parties (
+	id int auto_increment primary key,
+    user_id int,
+    party_id int,
+    constraint user_fk_parties foreign key (user_id) references users (id)
+    on delete cascade on update cascade,
+    constraint party_fk_parties foreign key (party_id) references parties (id)
+    on delete cascade on update cascade
+);
+
+insert into users(username, password, steam_username, user_rol)values('IzanCartio', 'pswd123', 'Izancube44', 'ADMIN');
+insert into users(username, password, steam_username, user_rol)values('David', '321var', 'Daviduber', 'USER');
+insert into users(username, password, steam_username, user_rol)values('Oscar', 'spider88', 'McOscar', 'USER');
+
+insert into groups(videogame, members, user_id)values('Spider-man', 21, 1);
+insert into groups(videogame, members, user_id)values('Minecraft', 24, 11);
+insert into groups(videogame, members, user_id)values('League', 17, 21);
+
+insert into parties(members, messages)values(21, 421);
+insert into parties(members, messages)values(24, 730);
+insert into parties(members, messages)values(17, 355);
+
+insert into users_in_parties(user_id, party_id)values(1, 1);
+insert into users_in_parties(user_id, party_id)values(11, 11);
+insert into users_in_parties(user_id, party_id)values(21, 21);
+
+select * from users;
