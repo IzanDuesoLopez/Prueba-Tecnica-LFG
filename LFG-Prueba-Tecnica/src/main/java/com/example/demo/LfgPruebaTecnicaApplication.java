8de2af0a9ca032a50e319d4adb5df1a246e2cea7
IzanDuesoLopez <izandueso@gmail.com>
2022-08-23 09:27:05 +0200

    Starter project

---
+++ LFG-Prueba-Tecnica/src/main/java/com/example/demo/LfgPruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfgPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfgPruebaTecnicaApplication.class, args);
+	}
+
+}
